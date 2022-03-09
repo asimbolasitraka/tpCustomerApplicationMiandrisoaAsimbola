7699091f8fc74b4782a5aad0342dd38783f5ae7f
asimbolasitraka <asimbola.sitraka@gmail.com>
2022-03-09 21:27:51 +0300

    Ajout EJB CustomerManager

---
+++ src/main/java/mg/ms/tpcustomerapplicationmiandrisoaasimbola/session/CustomManager.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package mg.ms.tpcustomerapplicationmiandrisoaasimbola.session;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import mg.ms.tpcustomerapplicationmiandrisoaasimbola.entities.Customer;
+
+
+/**
+ * Gère la persistance des Customers.
+ */
+@Stateless
+public class CustomManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+    @Resource
+    private javax.transaction.UserTransaction utx;
+
+    public void persist(Customer customer) {
+        try {
+            utx.begin();
+            em.persist(customer);
+            utx.commit();
+        } catch (IllegalStateException | SecurityException | HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 
+     * Récuperer tous les customers
+     * @return 
+     */
+    public List<Customer> getAllCustomers() {
+       Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList();
+    }
+
+    /**
+     * Modifier un customer
+     * @param customer
+     * @return 
+     */
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+     
+}
